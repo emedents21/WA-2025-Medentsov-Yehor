8029c662efb3e6b82473d856560bd802babc5a3d
emedents21 <emedentsbusiness@gmail.com>
2025-04-26 00:00:48 +0200

    26.04

--- Web01Cvic/03-php-projekt/app/models/Book.php
+++ Web01Cvic/03-php-projekt/app/models/Book.php
@@ -7,13 +7,14 @@ class Book {
         $this->db = $db;
     }
 
-    public function create($title, $author, $category, $subcategory, $year, $price, $isbn, $description, $link, $images) {
-        
-        // Dvojtečka označuje pojmenovaný parametr => Místo přímých hodnot se používají placeholdery.
-        // PDO je pak nahradí skutečnými hodnotami při volání metody execute().
-        // Chrání proti SQL injekci (bezpečnější než přímé vložení hodnot).
-        $sql = "INSERT INTO books (title, author, category, subcategory, year, price, isbn, description, link, images) 
-                VALUES (:title, :author, :category, :subcategory, :year, :price, :isbn, :description, :link, :images)";
+    public function create($title, $author, $category, $subcategory, $year, $price, $isbn, $description, $link, $images, $user_id) {
+
+        // Vkládáme i user_id, abychom měli vazbu na uživatele
+        $sql = "INSERT INTO books (
+                    title, author, category, subcategory, year, price, isbn, description, link, images, user_id
+                ) VALUES (
+                    :title, :author, :category, :subcategory, :year, :price, :isbn, :description, :link, :images, :user_id
+                )";
         
         $stmt = $this->db->prepare($sql);
         
@@ -27,7 +28,8 @@ class Book {
             ':isbn' => $isbn,
             ':description' => $description,
             ':link' => $link,
-            ':images' => json_encode($images) // Ukládání obrázků jako JSON
+            ':images' => json_encode($images),
+            ':user_id' => $user_id
         ]);
     }
 
@@ -37,6 +39,7 @@ class Book {
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
+
     public function getById($id) {
         $sql = "SELECT * FROM books WHERE id = :id";
         $stmt = $this->db->prepare($sql);
@@ -71,9 +74,13 @@ class Book {
             ':link' => $link
         ]);
     }
+
     public function delete($id) {
         $sql = "DELETE FROM books WHERE id = :id";
         $stmt = $this->db->prepare($sql);
         return $stmt->execute([':id' => $id]);
     }
+
+
+
 }
